aa016a84fc442e42ce4d49e14bb674c1599879e6
gujju0 <20ce03vatsal@gmail.com>
2023-10-22 14:10:46 +0530

    init

---
+++ js/questions.js
@@ -0,0 +1,81 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Which of the following monitor looks like a television and are normally used with non-portable computer systems?",
+    answer: "Mango",
+    options: [
+      "AV001-B",
+      "AV108-D",
+      "AV007-C",
+      "AV114-B"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which of the following is the smallest unit of data in a computer?",
+    answer: "Dal dhokli",
+    options: [
+      "AV002-C",
+      "AV109-B",
+      "AV006-A",
+      "AV113-D"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which of the following unit is responsible for converting the data received from the user into a computer understandable format?",
+    answer: "Dabeli",
+    options: [
+      "AV003-D",
+      "AV110-B",
+      "AV005-A",
+      "AV112-C"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which of the following is not a type of computer code?",
+    answer: "Parrot",
+    options: [
+      "AV004-D",
+      "AV011-B",
+      "AV104-D",
+      "AV111-B"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which of the following is designed to control the operations of a computer?",
+    answer: "Paratha",
+    options: [
+      "AV009-D",
+      "AV102-C",
+      "AV013-A",
+      "AV106-B"
+    ]
+  },
+ 
+    {
+    numb: 6,
+    question: "Which of the following are physical devices of a computer?",
+    answer: "Banana",
+    options: [
+      "AV010-C",
+      "AV103-C",
+      "AV012-D",
+      "AV105-B"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following devices provides the communication between a computer and the outer world?",
+    answer: "Pizza",
+    options: [
+      "AV004-A",
+      "AV011-C",
+      "AV104-A",
+      "AV111-D"
+    ]
+  },
+];
